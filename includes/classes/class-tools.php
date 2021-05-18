f35b1ff27d30bf6d74764cd6b601db107d9cbd3d
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2021-05-18 16:28:24 +0600

    improved

--- includes/classes/class-tools.php
+++ includes/classes/class-tools.php
@@ -271,30 +271,33 @@
 
 
         public function setup_fields( $directory = '' ) {
-                $directory      = $directory ? $directory : $this->default_directory;
-                $fields         = directorist_get_form_fields_by_directory_type( 'id', $directory );
-                foreach( $fields as $field ){
-                $field_key  = !empty( $field['field_key'] ) ? $field['field_key'] : '';
-                $label      = !empty( $field['label'] ) ? $field['label'] : '';
-                if( 'tax_input[at_biz_dir-location][]'  == $field_key ) {  $field_key = 'location'; }
-                if( 'admin_category_select[]'           == $field_key ) {  $field_key = 'category';  }
-                if( 'tax_input[at_biz_dir-tags][]'      == $field_key ) { $field_key = 'tag'; }
-                
-                if ( isset( $field['widget_name'] ) ) {
-                    if( 'pricing' == $field['widget_name'] ) {  
-                        $this->importable_fields[ 'price' ] = 'Price';
-                        $this->importable_fields[ 'price_range' ] = 'Price Range';
-                        continue;
+            $directory = $directory ? $directory : $this->default_directory;
+            $fields    = directorist_get_form_fields_by_directory_type( 'id', $directory );
+
+            if ( ! empty( $fields ) && is_array( $fields ) ) {
+                foreach( $fields as $field ) {
+                    $field_key  = !empty( $field['field_key'] ) ? $field['field_key'] : '';
+                    $label      = !empty( $field['label'] ) ? $field['label'] : '';
+                    if( 'tax_input[at_biz_dir-location][]'  == $field_key ) {  $field_key = 'location'; }
+                    if( 'admin_category_select[]'           == $field_key ) {  $field_key = 'category';  }
+                    if( 'tax_input[at_biz_dir-tags][]'      == $field_key ) { $field_key = 'tag'; }
+                    
+                    if ( isset( $field['widget_name'] ) ) {
+                        if( 'pricing' == $field['widget_name'] ) {  
+                            $this->importable_fields[ 'price' ] = 'Price';
+                            $this->importable_fields[ 'price_range' ] = 'Price Range';
+                            continue;
+                            }
+                        if( 'map' == $field['widget_name'] ) {  
+                            $this->importable_fields[ 'manual_lat' ] = 'Map Latitude';
+                            $this->importable_fields[ 'manual_lng' ] = 'Map Longitude';
+                            $this->importable_fields[ 'hide_map' ]   = 'Hide Map';
+                            continue;
                         }
-                    if( 'map' == $field['widget_name'] ) {  
-                        $this->importable_fields[ 'manual_lat' ] = 'Map Latitude';
-                        $this->importable_fields[ 'manual_lng' ] = 'Map Longitude';
-                        $this->importable_fields[ 'hide_map' ]   = 'Hide Map';
-                        continue;
                     }
+                    
+                    apply_filters( 'directorist_importable_fields', $this->importable_fields[ $field_key ] = $label );
                 }
-                
-                apply_filters( 'directorist_importable_fields', $this->importable_fields[ $field_key ] = $label );
             }
         }
 
