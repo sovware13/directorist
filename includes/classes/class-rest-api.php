e95a78915d502392c4d4cda65a010d13fdd44362
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2021-10-14 08:47:39 +0600

    added password reset api

--- includes/classes/class-rest-api.php
+++ includes/classes/class-rest-api.php
@@ -28,6 +28,54 @@ class ATBDP_Rest_API {
             ],
             'permission_callback' => '__return_true'
         ]);
+
+        // Send Email Link
+        register_rest_route( 'directorist/dev', '/reset-user-password/', [
+            'methods' => 'POST',
+            'callback' => [ $this, 'reset_user_password' ],
+            'args' => [
+                'email' => [
+                  'validate_callback' => function($param, $request, $key) {
+                    return is_email( $param );
+                  }
+                ],
+                'password' => [
+                  'validate_callback' => function($param, $request, $key) {
+                    return is_string( $param );
+                  }
+                ],
+                'pin' => [
+                  'validate_callback' => function($param, $request, $key) {
+                    return is_numeric( $param );
+                  }
+                ],
+            ],
+            'permission_callback' => '__return_true'
+        ]);
+
+        // Change Password
+        register_rest_route( 'directorist/dev', '/change-password/', [
+            'methods' => 'POST',
+            'callback' => [ $this, 'change_password' ],
+            'args' => [
+                'user_id' => [
+                  'validate_callback' => function($param, $request, $key) {
+                    return is_int( $param );
+                  }
+                ],
+                'old_password' => [
+                  'validate_callback' => function($param, $request, $key) {
+                    return is_string( $param );
+                  }
+                ],
+                'new_password' => [
+                  'validate_callback' => function($param, $request, $key) {
+                    return is_string( $param );
+                  }
+                ],
+            ],
+            'permission_callback' => '__return_true'
+        ]);
     }
 
     // Register User
@@ -60,7 +108,7 @@ class ATBDP_Rest_API {
         // Validate | Password
         if ( empty( $form_data['user_pass'] ) ) {
             $status['errors']['password_required'] = [
-                __('Password is required')
+                __('Password is required', 'directorist')
             ];
         }
 
@@ -103,10 +151,172 @@ class ATBDP_Rest_API {
     // Send password reset link
     public function send_password_reset_link( WP_REST_Request $request ) {
         $status = [
-            'success' => true,
-            'message' => 'Looks good',
-            'email'   => $request['email'],
+            'success'      => false,
+            'message'      => '',
+            'errors'       => [],
         ];
+
+        if ( empty( $request['email'] ) ) {
+            $status['success'] = false;
+            $status['errors']['email_required'] = __("Email is required", 'directorist');
+            $status['message'] = $status['errors']['email_required'];
+
+            return $status;
+        }
+
+        $user = get_user_by( 'email', $request['email'] );
+
+        // Validate User
+        if ( ! $user ) {
+            $status['success'] = false;
+            $status['errors']['invalid_user'] = __('User doesn\'t exists', 'directorist');
+            $status['message'] = $status['errors']['invalid_user'];
+
+            return $status;
+        }
+
+        ATBDP()->email->send_password_reset_pin_email( $request['email'] );
+
+        $status['success'] = true;
+        $status['message'] = __('The Password reset code has been sent to your email', 'directorist');
+
+        return $status;
+    }
+
+    // Reset User Password
+    public function reset_user_password( WP_REST_Request $request ) {
+        $status = [
+            'success'      => false,
+            'message'      => '',
+            'errors'       => [],
+        ];
+
+        // Validate Email
+        if ( empty( $request['email'] ) ) {
+            $status['success'] = false;
+            $status['errors']['email_required'] = __("Email is required", 'directorist');
+            $status['message'] = $status['errors']['email_required'];
+
+            return $status;
+        }
+
+        $user = get_user_by( 'email', $request['email'] );
+        
+        // Validate User
+        if ( ! $user ) {
+            $status['success'] = false;
+            $status['errors']['invalid_user'] = __('User doesn\'t exists', 'directorist');
+            $status['message'] = $status['errors']['invalid_user'];
+
+            return $status;
+        }
+
+        // Validate PIN
+        if ( empty( $request['pin'] ) ) {
+            $status['errors']['pin_required'] = __('The PIN is required', 'directorist');
+            $status['message'] = $status['errors']['pin_required'];
+
+            return $status;
+        }
+        
+        $email = $request['email'];
+        $transient_pin = get_transient( "directorist_reset_pin_$email" );
+
+        if ( $transient_pin != $request['pin'] ) {
+            $status['errors']['invalid_pin'] = __('The PIN is invalid or expired', 'directorist');
+            $status['message'] = $status['errors']['invalid_pin'];
+
+            return $status;
+        }
+
+        // Validate Password
+        if ( empty( $request['password'] ) ) {
+            $status['errors']['password_required'] = __('Password can\'t be empty', 'directorist');
+            $status['message'] = $status['errors']['password_required'];
+
+            return $status;
+        }
+
+        if ( ! empty( $request['password'] ) && strlen( $request['password'] ) < 5 ) {
+            $status['errors']['password_min_length_error'] = __('Password must be 5 chaercters', 'directorist');
+            $status['message'] = $status['errors']['password_min_length_error'];
+
+            return $status;
+        }
+        
+        // Change Password
+        wp_set_password( $request['new_password'], $user->ID );
+
+        // Delete The PIN
+        delete_transient( "directorist_reset_pin_$email" );
+
+        $status['success'] = true;
+        $status['message'] = __('Password has been changed successfully', 'directorist');
+
+        return $status;
+    }
+
+    // Change Password
+    public function change_password( WP_REST_Request $request ) {
+        $status = [
+            'success'      => false,
+            'message'      => '',
+            'errors'       => [],
+        ];
+
+        if ( empty( $request['username'] ) ) {
+            $status['success'] = false;
+            $status['errors']['invalid_user'] = __("Username is required", 'directorist');
+            $status['message'] = $status['errors']['invalid_user'];
+
+            return $status;
+        }
+
+        $user               = get_user_by( 'login', $request['username'] );
+        $has_valid_password = ( wp_check_password( $request['old_password'], $user->data->user_pass, $user->ID ) ) ? true : false;
+        $is_valid_user      = ( $user && $has_valid_password ) ? true : false;
+        
+        $status['is_valid_user'] = $is_valid_user;
+        $status['has_valid_password'] = $has_valid_password;
+        
+        // Validate User
+        if ( ! $user ) {
+            $status['success'] = false;
+            $status['errors']['invalid_user'] = __("User doesn't exists", 'directorist');
+            $status['message'] = $status['errors']['invalid_user'];
+
+            return $status;
+        }
+
+        // Validate Old Password
+        if ( ! $has_valid_password ) {
+            $status['success'] = false;
+            $status['errors']['invalid_old_password'] = __('Current Password is not valid', 'directorist');
+            $status['message'] = $status['errors']['invalid_old_password'];
+
+            return $status;
+        }
+
+        // Validate New Password
+        if ( empty( $request['new_password'] ) ) {
+            $status['errors']['password_required'] = __('New Password can\'t be empty', 'directorist');
+            $status['message'] = $status['errors']['password_required'];
+
+            return $status;
+        }
+
+        if ( ! empty( $request['new_password'] ) && strlen( $request['new_password'] ) < 5 ) {
+            $status['errors']['password_min_length_error'] = __('Password must be 5 chaercters', 'directorist');
+            $status['message'] = $status['errors']['password_min_length_error'];
+
+            return $status;
+        }
+        
+        // Change Password
+        wp_set_password( $request['new_password'], $user->ID );
+
+        $status['success'] = true;
+        $status['message'] = __('Password has been changed successfully', 'directorist');
 
         return $status;
     }
